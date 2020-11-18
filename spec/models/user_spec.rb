921d3121a102d74cb65c12540e3791c4d5be00f9
バントウフウマ <coolishpoo3@gmail.com>
2020-11-18 21:38:25 +0900

    add validation test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.new(name:"Example User",email:"user@example.com")
+  end
+
+  it "is valid" do
+    @user.valid?
+    expect(@user).to be_valid
+  end
+
+  it "is invalid without name" do
+    @user.name = '  '
+    @user.valid?
+    expect(@user).to_not be_valid
+  end
+
+  it "is invalid without email" do
+    @user.email = '  '
+    @user.valid?
+    expect(@user).to_not be_valid
+  end
+
+  it "is invalid with name too long" do
+    @user.name = "a"*51
+    @user.valid?
+    expect(@user).to_not be_valid
+  end
+
+  it "is invalid with email too long" do
+    @user.email = "a"*244 + "@example.com"
+    @user.valid?
+    expect(@user).to_not be_valid
+  end
+
+  #有効なメールフォーマットをテストする
+  it "accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US_ER@foo.bar.org
+                  first.last@foo.jp alice+bob@baz.cn] 
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      @user.valid?
+      expect(@user).to be_valid,"#{valid_address.inspect} should be valid"
+    end
+  end
+
+  #メールフォーマットのバリデーションテスト
+  it "reject invalid address" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                  foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      @user.valid?
+      expect(@user).to_not be_valid,"#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  #重複するメールアドレスは登録できないようになっているかテスト(大文字、小文字関係なし)
+  it "has unique email address" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    duplicate_user.valid?
+    expect(duplicate_user).to_not be_valid
+  end
+
 end
