3318f708974b026411edb6ffa773fe6bc29cdb0e
バントウフウマ <coolishpoo3@gmail.com>
2020-11-23 10:39:12 +0900

    define create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
+    
+  end
+
+  def create
+    user = User.find_by(email:params[:session][:email].downcase,)
+    if user && user.authenticate(params[:session][:password])
+      #ログイン成功の処理を実装
+    else
+      flash[:danger] = 'ログインできませんでした正しい情報を入力してください'
+      render 'new'
+    end
+  end
+
+  def destroy
+
   end
 end
