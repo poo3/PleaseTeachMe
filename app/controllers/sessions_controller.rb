0f42d46498ef42b6e5443efd1e4da51c15c3479f
バントウフウマ <coolishpoo3@gmail.com>
2020-11-24 19:45:13 +0900

    add test for user_login and user_logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email:params[:session][:email].downcase,)
     if user&.authenticate(params[:session][:password])
       log_in user
+      flash[:success] = 'ログインに成功しました！'
       redirect_to user
     else
       flash.now[:danger] = 'ログインできませんでした正しい情報を入力してください'
--- spec/system/users_logins_spec.rb
+++ spec/system/users_logins_spec.rb
@@ -14,7 +14,46 @@ RSpec.describe "UsersLogins", type: :system do
       visit root_path
       expect(page).to_not have_selector 'div.alert'
     end
+  end
 
+  context "有効な情報でユーザログインする時" do
+    before do
+      #ユーザを作成する
+      User.create!(name: 'test_user',email: 'example@test.co.jp',password: 'test1234567890',
+                  password_confirmation: 'test1234567890')
+    end
+    scenario "ログインする" do
+      visit login_path
+      expect(page).to have_content 'Log in'
+      fill_in 'session_email', with: 'example@test.co.jp'
+      fill_in 'session_password', with: 'test1234567890'
+      click_button 'ログイン'
+      #ログイン後ユーザ詳細画面へリダイレクトしているか確認
+      expect(page).to have_content 'ログアウト'
+      expect(page).to have_content 'ログインに成功しました！'
+    end
+  end
+
+  context "ユーザがログインしている時" do 
+    before do
+      #ユーザを作成する
+      User.create!(name: 'test_user',email: 'example@test.co.jp',password: 'test1234567890',
+        password_confirmation: 'test1234567890')
+      #ログインする
+      visit login_path
+      expect(page).to have_content 'Log in'
+      fill_in 'session_email', with: 'example@test.co.jp'
+      fill_in 'session_password', with: 'test1234567890'
+      click_button 'ログイン'
+    end
+
+    scenario "ログアウトする" do
+      visit root_path
+      click_link 'ログアウト'
+      #ログアウト後にホーム画面に戻っているかチェック
+      expect(page).to have_content 'ログイン'
+      expect(page).to have_selector 'div.introduction-app-container'
+    end
   end
 end
 
