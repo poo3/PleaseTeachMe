d72152810aa9645d45de53b18185cf32404dd04e
バントウフウマ <coolishpoo3@gmail.com>
2020-12-03 21:43:33 +0900

    add function for user edit,update action

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -32,6 +32,11 @@ module SessionsHelper
     end
   end
 
+  #渡されたユーザがセッション中のユーザだったらtrueを返す
+  def current_user?(user)
+    user && user == current_user
+  end
+
   #ユーザがログインしているならtrue、していなければfalseを返す
   def logged_in?
     !current_user.nil?
