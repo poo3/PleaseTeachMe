da058a4175dd0e988e86222a14102540a847f1f0
バントウフウマ <coolishpoo3@gmail.com>
2020-12-07 15:24:04 +0900

    modification home.js

--- app/javascript/custom/home.js
+++ app/javascript/custom/home.js
@@ -1,17 +1,17 @@
-window.onload = function(){
-  console.log('ready');
-  var user_nav_button = document.getElementById('user-nav-button');
-  console.log(user_nav_button);
-  user_nav_button.onclick = function() {
-    userNavToggle();
-  };
-  function userNavToggle(){
-    var user_nav = document.getElementById('user-nav');
-    console.log(user_nav.style.display)
+document.addEventListener("turbolinks:load", function() {
+  const button = document.getElementById("user-nav-button");
+  document.getElementById('user-nav').style.display = 'none'
+  // ボタンをクリックしたらナビゲーションの表示の切り替え
+  button.addEventListener("click", event => {
+    var user_nav = document.getElementById('user-nav')
+    userNavToggle(user_nav)
+  });
+  // ユーザナビ表示切り替えの関数
+  function userNavToggle(user_nav){
     if(user_nav.style.display === 'none'){
       user_nav.style.display = 'flex';
     } else {
       user_nav.style.display = 'none';
     }
   };
-};
+});
