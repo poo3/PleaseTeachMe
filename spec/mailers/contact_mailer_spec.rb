3f4233b27e0cf8d12fc62984a100455a6d1ecaa7
バントウフウマ <coolishpoo3@gmail.com>
2020-12-10 18:56:05 +0900

    add test

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe ContactMailer, type: :mailer do
     it "sends contact email to the setting email" do
       expect(mail.to).to eq(["applicationtestpoo3@gmail.com"])
     end
+
+    #ユーザからのお問い合わせに必要な情報が入力されているか
+    it "sends must information in email text" do
+      expect(contact.name).to_not eq(nil)
+      expect(contact.email).to_not eq(nil)
+      expect(contact.content).to_not eq(nil)
+    end
   end
 end
