69af4386e0a2a96140cae8df23bc5044df9ec820
バントウフウマ <coolishpoo3@gmail.com>
2020-12-15 17:25:35 +0900

    setting production for using gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,17 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # GmailのActionMailer設定
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              Rails.application.credentials.gmail[:address],
+    port:                 Rails.application.credentials.gmail[:port],
+    domain:               Rails.application.credentials.gmail[:domain],
+    user_name:            Rails.application.credentials.gmail[:user_name],
+    password:             Rails.application.credentials.gmail[:password],
+    authentication:       Rails.application.credentials.gmail[:authentication],
+    enable_starttls_auto: true }
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
