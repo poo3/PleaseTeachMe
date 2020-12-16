ea466b26aea96f67518558ee5806bd869f34d5c6
バントウフウマ <coolishpoo3@gmail.com>
2020-12-16 19:34:17 +0900

    add thanks_for_contacting

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,4 +5,9 @@ class ContactMailer < ApplicationMailer
     @contact = contact
     mail to:"applicationtestpoo3@gmail.com",subject: "【PleaseTeachMe】ユーザからのお問い合わせ"
   end
+
+  def thanks_for_contacting(contact)
+    @contact = contact
+    mail to:contact.email,subject: "【PleaseTeachMe】お問い合わせありがとうございます"
+  end
 end
