9183ae4bc942cf70e8f39b4fdc09f7f4af893442
バントウフウマ <coolishpoo3@gmail.com>
2020-12-23 11:31:14 +0900

    create seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  email: 'example@example.com',
+  name: 'テストユーザ',
+  password: 'test1234',
+  password_confirmation: 'test1234'
+)
