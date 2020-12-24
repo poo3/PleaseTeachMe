30cac249ecc590070aad61abdf61a9d2adf9b0e7
バントウフウマ <coolishpoo3@gmail.com>
2020-12-24 19:47:54 +0900

    add question seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,10 @@ User.create!(
                password:              password,
                password_confirmation: password)
 end
+
+# ユーザーの一部を対象にマイクロポストを生成する
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.questions.create!(content: content) }
+end
