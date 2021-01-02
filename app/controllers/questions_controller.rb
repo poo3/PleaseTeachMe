077943856d01bef288ca9f3d603d54ded7c15e3a
バントウフウマ <coolishpoo3@gmail.com>
2021-01-02 17:24:52 +0900

    add action questions delete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :logged_in_user, only: [:new,:create,:destroy]
+  before_action :correct_user,   only: :destroy
 
   def new
     @question = Question.new
@@ -20,12 +21,19 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-
+    @question.destroy
+    flash[:success] = "質問を削除しました"
+    redirect_to user_path(current_user)
   end
 
   private
 
     def question_params
       params.require(:question).permit(:content,:title)
+    end
+
+    def correct_user
+      @question = current_user.questions.find_by(id: params[:id])
+      redirect_to root_url if @question.nil?
     end
 end
