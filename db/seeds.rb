4518e74e58254dda7e2e337096f685b5c239bce5
バントウフウマ <coolishpoo3@gmail.com>
2021-03-01 21:00:35 +0900

    modification create sample user for user_type

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,45 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# メインサンプルユーザ
+# メインサンプルユーザ(生徒)
 User.create!(
-  email: 'example@example.com',
+  email: 'example-student@example.com',
   name: 'テストユーザ',
   password: 'password',
-  password_confirmation: 'password'
+  password_confirmation: 'password',
+  user_type: 'student'
+)
+# メインサンプルユーザ(教師)
+User.create!(
+  email: 'example-teacher@example.com',
+  name: 'テストユーザ',
+  password: 'password',
+  password_confirmation: 'password',
+  user_type: 'teacher'
 )
 
-# 追加のユーザーをまとめて生成する
+# 追加のユーザーをまとめて生成する(生徒)
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@example.com"
+  email = "example-student-#{n+1}@example.com"
   password = "password"
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               user_type: 'student')
+end
+
+# 追加のユーザーをまとめて生成する(教師)
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-teacher-#{n+1}@example.com"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               user_type: 'teacher')
 end
 
 # ユーザーの一部を対象にマイクロポストを生成する
