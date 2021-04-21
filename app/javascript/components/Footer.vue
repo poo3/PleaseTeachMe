40be16b4463e6cfdd01ea48f1618a1218b198b28
バントウフウマ <coolishpoo3@gmail.com>
2021-04-21 21:29:40 +0900

    add Footer.vue component

---
+++ app/javascript/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <div class="copy-right">©︎ 2020 Please Teach Me, All rights reserved</div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.copy-right {
+  text-align: center;
+  color: black;
+  font-size: 1.5rem;
+}
+</style>
