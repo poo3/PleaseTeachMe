a2dcc529820c6e8b95ede79310f4a9af26679ee5
バントウフウマ <coolishpoo3@gmail.com>
2021-04-22 10:08:23 +0900

    add Home.vue

---
+++ app/javascript/components/staticpages/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="introduction-app">
+    <h1>リモート自習塾、<br />好きな時に先生に質問</h1>
+  </div>
+</template>
+
+<script>
+import "PleaseTeachMe-background-home.png";
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.introduction-app {
+  background-image: url("~PleaseTeachMe-background-home.png");
+  background-repeat: no-repeat;
+  width: 100%;
+  margin: auto;
+  height: 80vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  > h1 {
+    color: whitesmoke;
+    width: 100%;
+  }
+}
+</style>
