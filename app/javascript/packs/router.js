8d215f18287bda0e4a76820612a76acc808c0c6c
バントウフウマ <coolishpoo3@gmail.com>
2021-04-22 10:09:38 +0900

    setting app routing

--- app/javascript/packs/router.js
+++ app/javascript/packs/router.js
@@ -1,12 +1,18 @@
 import Vue from "vue/dist/vue.esm";
 import VueRouter from "vue-router";
-import Index from "../app";
+import Home from "../components/staticpages/Home";
+import Help from "../components/staticpages/Help";
+import Contact from "../components/staticpages/Contact";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   mode: "history",
-  routes: [{ path: "/", component: Index, name: "root_path" }],
+  routes: [
+    { path: "/", component: Home, name: "home_path" },
+    { path: "/help", component: Help, name: "help_path" },
+    { path: "/contact", component: Contact, name: "contact_path" },
+  ],
 });
 
 export default router;
