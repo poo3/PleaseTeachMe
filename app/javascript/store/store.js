7d411c1dedab346a35b1b406fc2bd1bf8ba8a59d
バントウフウマ <coolishpoo3@gmail.com>
2021-04-26 16:41:47 +0900

    define store.js

---
+++ app/javascript/store/store.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import router from "../packs/router";
+import axios from "axios";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {},
+  mutations: {},
+});
