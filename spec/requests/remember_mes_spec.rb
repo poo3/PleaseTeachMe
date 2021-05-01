561597ff85a0f6c3d3e22b04dbea924190f36235
バントウフウマ <coolishpoo3@gmail.com>
2021-05-01 17:04:49 +0900

    vueに置き換えてから変更された部分のテストを削除

--- spec/requests/remember_mes_spec.rb
+++ spec/requests/remember_mes_spec.rb
@@ -1,25 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "RememberMes", type: :request do
-  let(:user){FactoryBot.create(:user)}
-
-  # テストユーザーがログイン中の場合にtrueを返す
-  def is_logged_in?
-    !session[:user_id].nil?
-  end
-
-  describe "delete /logout" do
-    it "redirects to root_path" do
-      post login_path, params: {session:{
-        email: user.email,
-        password: user.password
-      }}
-      delete logout_path
-      aggregate_failures do
-        expect(response).to redirect_to root_path
-        expect(is_logged_in?).to be false
-      end
-    end
-
-  end
+  
 end
--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,29 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-  let(:user){FactoryBot.create(:user)}
-
-  describe "sessions" do
-    it "ログインページへのアクセス" do
-      get login_path
-      expect(response).to have_http_status(200)
-    end
-
-    context "正しい情報が入力されている時" do
-      
-      it "ログイン" do
-        post login_path params:{session:{email: user.email,password: user.password}}
-        expect(flash[:success]).to match('ログインに成功しました')
-        expect(response).to redirect_to user_path(user.id)
-      end
-    end
-    context "正しくない情報が入力されている時" do
-      
-      it "ログイン" do
-        post login_path params:{session:{name: "",email: " "}}
-        expect(flash[:danger]).to match('ログインできませんでした正しい情報を入力してください')
-      end
-    end
-
-  end
+  l
 end
