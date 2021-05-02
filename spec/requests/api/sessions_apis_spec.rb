cfb92b1f93af50169222228280ae74eac248183a
バントウフウマ <coolishpoo3@gmail.com>
2021-05-03 00:52:54 +0900

    ログインセッションの作成が成功するリクエストスペックを追加

---
+++ spec/requests/api/sessions_apis_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::SessionsApis', type: :request do
+  describe 'POST /login' do
+    user = FactoryBot.create(:user)
+    let(:session_params) do
+      { session: { email: user.email, password: user.password } }
+    end
+    it 'セッション作成が成功すること' do
+      post '/login', params: session_params
+      expect(response).to have_http_status(:created)
+      json = JSON.parse(response.body)
+      expect(json['user']).to include(
+        {
+          'id' => be_present,
+          'name' => user.name,
+          'email' => user.email,
+          'user_type' => user.user_type,
+        },
+      )
+      expect(json).to include('message')
+    end
+  end
+end
