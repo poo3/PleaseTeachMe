8585c5b901036d3a50e8731ad9510b37a199c262
バントウフウマ <coolishpoo3@gmail.com>
2021-05-02 23:46:00 +0900

    メールアドレスはユニークな値の為にシーケンスを追加しました

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    name { "test_user" }
-    email { "example@test.co.jp" }
-    password {"test1234567890"}
-    password_confirmation {"test1234567890"}
-    user_type { "student" }
+    name { 'test_user' }
+    sequence(:email) { |n| "tester#{n}@example.com" }
+    password { 'test1234567890' }
+    password_confirmation { 'test1234567890' }
+    user_type { 'student' }
   end
 end
