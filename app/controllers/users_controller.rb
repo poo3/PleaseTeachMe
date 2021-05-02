3febf01f28afbc811bd7e4c4ddd30355aeca0f2d
バントウフウマ <coolishpoo3@gmail.com>
2021-05-02 19:52:01 +0900

    ユーザの保存に失敗した時例外処理ではなくifで処理を分けました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit,:update]
-  before_action :correct_user, only: [:edit,:update]
+  before_action :logged_in_user, only: %i[edit update]
+  before_action :correct_user, only: %i[edit update]
 
   def new
     @user = User.new
   end
 
   def index
-    @user = User.find(params[:id]).select( :name,:user_type)
+    @user = User.find(params[:id]).select(:name, :user_type)
     render json: @user
   end
 
@@ -16,19 +16,30 @@ class UsersController < ApplicationController
     # @questions = @user.questions.paginate(page:params[:page], per_page:9)
     @user = User.find(params[:id])
     if current_user?(@user)
-      render json: {user: @user,current_user: true}
+      render json: { user: @user, current_user: true }
     else
-      render template: "sessions/new", json:{message: "ログインして下さい",current_user: false}
+      render template: 'sessions/new',
+             json: {
+               message: 'ログインして下さい',
+               current_user: false,
+             }
     end
   end
 
   def create
     user = User.new(user_params)
-    if user.save!
+    if user.save
       log_in user
-      render json: {user: user,message: "#{user.name}様ようこそPleaseTeachmeへ"}, status: :created
+      render json: {
+               user: user,
+               message: "#{user.name}様ようこそPleaseTeachmeへ",
+             },
+             status: :created
     else
-      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      render json: {
+               errors: user.errors.full_messages,
+             },
+             status: :unprocessable_entity
     end
     # @user = User.new(user_params)
     # begin
@@ -49,7 +60,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:success] = "プロフィール更新しました！"
+      flash[:success] = 'プロフィール更新しました！'
       redirect_to @user
     else
       render 'edit'
@@ -59,12 +70,12 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation,:user_type)
+    params
+      .require(:user)
+      .permit(:name, :email, :password, :password_confirmation, :user_type)
   end
-  
-  # beforeアクション
 
+  # beforeアクション
 
   #正しいユーザか確認する（セッションユーザ本人のページかどうか）
   def correct_user
