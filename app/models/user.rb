a6a13d9dbe30ce48e1e49da473b5a64f40e37fd1
バントウフウマ <coolishpoo3@gmail.com>
2021-05-04 17:03:23 +0900

    user_typeのバリデーションを追加しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
               uniqueness: true
   has_secure_password
   validates :password,presence: true,length:{minimum: 8},allow_nil: true
-  validates :user_type,presence: true
+  validates :user_type,presence: true,inclusion:{in: %w(teacher student),message: "%{value}は無効な値です"}
 
   class << self
     # 渡された文字列のハッシュ値を返す
