a3d5b3477b1c1a651ee7bef0106a53e78172780d
バントウフウマ <coolishpoo3@gmail.com>
2021-05-07 18:50:09 +0900

    ナビゲーションガード時に発行されるリクエストのテストを追加

--- spec/requests/api/sessions_apis_spec.rb
+++ spec/requests/api/sessions_apis_spec.rb
@@ -34,4 +34,34 @@ RSpec.describe 'Api::SessionsApis', type: :request do
       expect(json).to include('message')
     end
   end
+  
+  describe "POST /api/auth_conf" do
+    user = FactoryBot.create(:user)
+    another_user = FactoryBot.create(:user)
+    let :unknown_user_id do 99999 end
+    let(:session_params) do
+      { session: { email: user.email, password: user.password } }
+    end
+    let(:another_session_params) do
+      { session: { email: another_user.email, password: another_user.password } }
+    end
+    before do
+      post '/api/login', params: session_params
+    end
+    it "認証に成功すること" do
+      post '/api/auth_conf',params:{id: user.id}
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "認証に失敗すること(存在しないユーザIDとして実行)" do
+      post '/api/auth_conf',params:{id: unknown_user_id}
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "認証に失敗すること（存在するユーザIDとして実行）" do
+      post '/api/login', params: another_session_params
+      post '/api/auth_conf',params:{id: user.id}
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
 end
