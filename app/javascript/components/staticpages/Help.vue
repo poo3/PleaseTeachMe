f85f5772bda124094c308f280fe4196058418e3e
バントウフウマ <coolishpoo3@gmail.com>
2021-05-10 21:53:56 +0900

    ヘルプページの雛形作成しました

--- app/javascript/components/staticpages/Help.vue
+++ app/javascript/components/staticpages/Help.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>Hello This is Help page</h1>
+  <div class="helpwrapper">
+    <h1>Q＆A</h1>
+    <v-expansion-panels>
+      <v-expansion-panel v-for="(item, i) in 5" :key="i">
+        <v-expansion-panel-header>{{
+          listQuestionArray[i]
+        }}</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          {{ listAnswerArray[i] }}</v-expansion-panel-content
+        >
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      listQuestionArray: [
+        "Q1の質問です",
+        "Q2の質問です",
+        "Q3の質問です",
+        "Q4の質問です",
+        "Q5の質問です",
+      ],
+      listAnswerArray: ["1111", "2222", "3333", "4444", "5555"],
+    };
+  },
+};
+</script>
