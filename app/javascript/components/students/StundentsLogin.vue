766ef49d11ae8745b6abc73e5ea85833bde009e4
バントウフウマ <coolishpoo3@gmail.com>
2021-05-18 17:26:23 +0900

    ログインする際にユーザがログインしようとしているuser_typeをpostするように変更

--- app/javascript/components/students/StundentsLogin.vue
+++ app/javascript/components/students/StundentsLogin.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="student-login-form-wraper">
     <h1>生徒ログイン</h1>
-    <users-login-form></users-login-form>
+    <users-login-form :user_type="user_type"></users-login-form>
   </div>
 </template>
 
@@ -9,6 +9,9 @@
 import UsersLoginForm from "../_users/UsersLoginForm";
 export default {
   components: { UsersLoginForm },
+  data() {
+    return { user_type: "student" };
+  },
 };
 </script>
 
