b5803e3696543ef0920f486f645d70972f5693a9
バントウフウマ <coolishpoo3@gmail.com>
2021-05-21 18:35:37 +0900

    pageTitleを表示するコンポーネントを追加

---
+++ app/javascript/components/PageTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page-title">
+    <h1>{{ pageTitle }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["pageTitle"],
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 3rem;
+  color: whitesmoke;
+}
+</style>
